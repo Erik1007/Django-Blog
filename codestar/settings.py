aaee614f72a634d22add613d2fe513b3a32e9d35
Erik1007 <erik.hartman1007@gmail.com>
2022-11-12 17:46:19 +0000

    final deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,6 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['eh-codestar-blog-22.herokuapp.com', 'localhost']
 
 
